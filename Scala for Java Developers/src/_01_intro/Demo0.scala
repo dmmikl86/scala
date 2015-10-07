5d32ed730037bc32c52c660d41512c9967095f50
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-10-07 15:25:02 +0300

    added some examples

--- Scala for Java Developers/src/_01_intro/Demo0.scala
+++ Scala for Java Developers/src/_01_intro/Demo0.scala
@@ -3,5 +3,20 @@ package _01_intro
 object Demo0 {
   def main(args: Array[String]) {
     println("Hello World!")
+
+    val arr = Array(0, 1, 2)
+    arr(1) = 0
+    val t = new T(2)
+    t.pr
+    val k:Int => Int = 1 + ;
+    println(k(5))
+
+    if (k(7) > 0){
+      print(3)
+    }
   }
+}
+
+class T (val vale: Int)extends AnyVal{
+  def pr = println(vale)
 }
